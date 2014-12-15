c78f80447003e3101d0540440ae644e387fa1038
Elger van Boxtel <elgervb@gmail.com>
2014-12-15 15:53:57 +0100

    initial commit

---
+++ src/js/app/app.js
@@ -0,0 +1,24 @@
+
+console.log('app.js');
+
+var app = angular.module('links', ['ngRoute'])
+
+.config(function($routeProvider, $locationProvider) {
+ 
+  $routeProvider
+  	.when('/', {
+	  controller: 'LinksController',
+	  templateUrl: '/assets/js/app/modules/links/links.html'
+	})
+
+   $locationProvider.html5Mode('true');
+
+}); // end config
+
+
+app.filter("hideEmpty", function(){ return function(object, query){
+    if(!query)
+        return {}
+    else
+        return object;
+}});
---
+++ src/js/app/modules/links/LinksController.js
@@ -0,0 +1,32 @@
+
+app.controller('LinksController', ['$scope', '$http', function($scope, $http){
+
+		$http.get('assets/js/app/modules/links/links.json')
+      .success(function(data, status, headers, config){
+        // get sorted links (by title)
+        $scope.links = data.sort(function compare(a,b) {
+          if (a.title < b.title)
+             return -1;
+          if (a.title > b.title)
+            return 1;
+          return 0;
+        })
+      });
+
+    $scope.redirect = function(link){
+      window.open(link.url, '_blank').focus();
+    };
+
+    $scope.getTags = function(tags){
+      console.log('get tags');
+      if (tags){
+        return tags.split(';');
+      } 
+    };
+
+    $scope.onKeyPress = function($event){
+      if ($event.keyCode === 27){
+        $scope.searchLinks = "";
+      }
+    };
+}]);
