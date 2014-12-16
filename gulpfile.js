0994cda3cf0c22b8779188b91d8046c8efb47e8b
elger <elgervb@gmail.com>
2014-12-16 23:14:51 +0100

    use gullp plumber

--- gulpfile.js
+++ gulpfile.js
@@ -25,6 +25,7 @@ var gulp = require('gulp'),
     gulpif = require('gulp-if'),
     todo = require('gulp-todo'),
     jsdoc = require("gulp-jsdoc"),
+    plumber = require('gulp-plumber'),
     ngannotate = require('gulp-ng-annotate');
 
 
@@ -126,6 +127,7 @@ gulp.task('express-lr', ['express', 'live-reload'], function(){});
  */
 gulp.task('images', function() {
   return gulp.src('src/img/**/*')
+    .pipe(plumber())
     .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
     .pipe(gulp.dest('dist/assets/img'))
     .pipe(notify({ message: 'Images task complete' }));
@@ -150,19 +152,19 @@ gulp.task('live-reload', ['watch'], function() {
 
 
 /**
- * Task to handle and deploy all javascript, application & vendor
- *
- * Depends on: scripts-app, scripts-vendor
- */
-gulp.task('scripts', ['scripts-app','scripts-vendor']);
-
-
-/**
  * Removes the node_modules
  */
 gulp.task('remove',['clean'], function(cb){
   del('node_modules', cb);
 });
+
+
+/**
+ * Task to handle and deploy all javascript, application & vendor
+ *
+ * Depends on: scripts-app, scripts-vendor
+ */
+gulp.task('scripts', ['scripts-app','scripts-vendor']);
 
 
 /**
@@ -173,11 +175,9 @@ gulp.task('remove',['clean'], function(cb){
  */
 gulp.task('scripts-app', ['docs'], function() {
   return gulp.src('src/js/app/**/*.js')
+    .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(jshint())
-    .on('error', notify.onError(function (error) {
-      return error.message;
-     }))
     .pipe(jshint.reporter(stylish))
     .pipe(concat('app.js'))
     .pipe(gulp.dest('dist/assets/js'))
@@ -185,10 +185,9 @@ gulp.task('scripts-app', ['docs'], function() {
     .pipe(gulpif(!argv.dev, stripDebug()))
     .pipe(ngannotate())
     .pipe(gulpif(!argv.dev, uglify()))
-    .on('error', handleError)
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('dist/assets/js'))
-    .pipe(notify({ message: 'Scripts app task complete' }));
+    .pipe(notify({ message: 'Scripts app task complete. Dev=' + !!argv.dev }));
 });
 
 
@@ -211,10 +210,9 @@ gulp.task('scripts-vendor', function() {
  */
 gulp.task('styles', function() {
   return gulp.src('src/styles/main.scss')
+    .pipe(plumber())
     .pipe(sass({ style: 'expanded' }))
-    .on('error', handleError)
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .on('error', handleError)
     .pipe(gulp.dest('dist/assets/css'))
     .pipe(rename({suffix: '.min'}))
     .pipe(minifycss())
@@ -228,10 +226,11 @@ gulp.task('styles', function() {
  */
 gulp.task('todo', function() {
     gulp.src('src/js/app/**/*.js')
-        .pipe(todo())
-        .pipe(gulp.dest('./')) //output todo.md as markdown
-        .pipe(todo.reporter('json', {fileName: 'todo.json'}))
-        .pipe(gulp.dest('./')) //output todo.json as json
+      .pipe(plumber())
+      .pipe(todo())
+      .pipe(gulp.dest('./')) //output todo.md as markdown
+      .pipe(todo.reporter('json', {fileName: 'todo.json'}))
+      .pipe(gulp.dest('./')) //output todo.json as json
 });
 
 
