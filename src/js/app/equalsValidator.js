9ccc8338a6e10c693436ca754ba01b2f5da9015a
elger <elgervb@gmail.com>
2015-01-29 23:24:42 +0100

    Register a user
    #19

---
+++ src/js/app/equalsValidator.js
@@ -0,0 +1,47 @@
+/**
+ * Validates that 2 models are equals.
+ *
+ * Use it as follows:
+ *
+ * <input type="text" name="password" equals-validator="{{passwordretype}}" />
+ * <input type="text" name="passwordretype" />
+ * 
+ * <div ng-show="form.$submitted ||  (form.password.$touched && form.passwordretype.$touched)">
+ *   <span ng-show="form.password.$error.equalsValidator">Passwords did not match.</span>
+ * </div>
+ */
+app.directive('equalsValidator', [
+  function() {
+
+    var link = function($scope, $element, $attrs, ctrl) {
+
+      var validate = function(viewValue) {
+        var comparisonModel = $attrs.equalsValidator;
+        
+        if(!viewValue || !comparisonModel){
+          // It's valid because we have nothing to compare against
+          ctrl.$setValidity('equalsValidator', true);
+          return viewValue;
+        }
+
+        // It's valid if model is equal to the model we're comparing against
+        ctrl.$setValidity('equalsValidator', viewValue == comparisonModel );
+        return viewValue;
+      };
+
+      ctrl.$parsers.unshift(validate);
+      ctrl.$formatters.push(validate);
+
+      $attrs.$observe('equalsValidator', function(comparisonModel){
+        return validate(ctrl.$viewValue);
+      });
+      
+    };
+
+    return {
+      require: 'ngModel',
+      link: link
+    };
+
+  }
+]);
