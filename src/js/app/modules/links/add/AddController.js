a25d13946adeb52be00e17f74a3aa610479486df
elger <elgervb@gmail.com>
2015-01-31 21:50:26 +0100

    when adding a link, all tags should be lower case
    fixes #10

--- src/js/app/modules/links/add/AddController.js
+++ src/js/app/modules/links/add/AddController.js
@@ -24,7 +24,7 @@ app.controller('AddController',
 
     //prevent dupes
     if ($scope.link.tags.indexOf(tag) === -1){
-      $scope.link.tags.push(tag);
+      $scope.link.tags.push(tag.toLowerCase());
       $scope.tag = "";
     }
   
