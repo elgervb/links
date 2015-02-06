945a1157c95e7ecd5873442730abacbb4e2e11d3
Elger van Boxtel <elgervb@gmail.com>
2015-02-06 20:39:25 +0100

    debug values

--- src/js/app/providers/values.js
+++ src/js/app/providers/values.js
@@ -1,1 +1,6 @@
-app.value('baseUrl', 'http://localhost/git/REST-api/');
+// debug
+//app.value('baseUrl', 'http://localhost/git/REST-api/');
+
+// prod
+app.value('baseUrl', 'http://elgervanboxtel.nl/REST-api/');
+
