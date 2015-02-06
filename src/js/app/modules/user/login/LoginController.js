740e9570c6ff755e9de741c0b6c1a8631900be9f
Elger van Boxtel <elgervb@gmail.com>
2015-02-06 14:38:46 +0100

    after succesfull login, redirect to /

--- src/js/app/modules/user/login/LoginController.js
+++ src/js/app/modules/user/login/LoginController.js
@@ -16,6 +16,7 @@ app.controller('LoginController',
      	function(user){ // success
      		console.dir(user);
         SettingsService.user(user);
+        $location.path('/');
      	},
      	function(){ // failure
      		$scope.error = true;
