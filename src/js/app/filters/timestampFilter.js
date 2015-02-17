5c7e58f32beb13b966cab6de205617c8035f0051
Elger van Boxtel <elgervb@gmail.com>
2015-02-17 13:14:54 +0100

    new timestamp filter

---
+++ src/js/app/filters/timestampFilter.js
@@ -0,0 +1,14 @@
+app.filter("timestampFilter", function(){ 
+  return function(object, query){
+    if (angular.isNumber(object)){
+      var result, date = new Date(object);
+      result = date.getFullYear();
+      result += "-"+date.getMonth();
+      result += "-"+date.getDate();
+      result += " "+date.getHours();
+      result += ":"+date.getMinutes();
+      return result;
+    }
+    return object;
+  };
+});
