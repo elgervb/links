cac809f4dbbf398de22f15dc9e99c81e120239ad
elger <elgervb@gmail.com>
2015-02-26 22:07:30 +0100

    implemented storageService

--- src/js/app/services/StorageService.js
+++ src/js/app/services/StorageService.js
@@ -10,15 +10,35 @@
  */
 app.service('StorageService', function(){
 
-	this.storage = window.localStorage;
+	var storage = window.localStorage;
 
-	var get = function(key){return this.storage.getItem(key);}, 
-	set = function(key, value){ this.storage.setItem(key, value); return this; }, 
-	key = function(index){return this.storage.key(index);}, 
-	remove = function(key){this.storage.removeItem(key); return this;}, 
-	clear = function(){this.storage.clear(); return this;},
+	var get = function(key){return angular.fromJson( storage.getItem(key) );}, 
+	set = function(key, value){ storage.setItem(key, angular.toJson(value) ); return this; }, 
+	key = function(index){return storage.key(index);}, 
+	remove = function(key){storage.removeItem(key); return this;}, 
+	clear = function(){storage.clear(); return this;},
 	create = function(key){
+		var _clear = function(){
+			clear(); return this;
+		},
+		_get = function(){
+			return get(key);
+		},
+		_set = function(value){
+			set(key, value); return this;
+		},
+		_remove = function(){
+			remove(key); return this;
+		};
 
+		return {
+			clear: _clear,
+			get: _get,
+			key: key,
+			lenght: storage.length,
+			set: _set,
+			remove: _remove
+		};
 	};
 
 	return {
@@ -26,7 +46,7 @@ app.service('StorageService', function(){
 		create : create,
 		get : get,
 		key : key,
-		length : this.storage.length,
+		length : storage.length,
 		set : set,
 		remove : remove
 	};
