f42ed7455ef89c90b558fa0c8d12115d8015e467
Elger van Boxtel <elgervb@gmail.com>
2015-05-26 12:04:47 +0200

    Timestamp always shows a date in 1970... #43
    fixed #43

--- src/js/app/filters/timestampFilter.js
+++ src/js/app/filters/timestampFilter.js
@@ -1,14 +1,23 @@
 app.filter('timestampFilter', function() { 
   return function(object, query) {
-    if (angular.isNumber(object)) {
-      var result, date = new Date(object);
-      result = date.getFullYear();
-      result += '-' + date.getMonth();
-      result += '-' + date.getDate();
-      result += ' ' + date.getHours();
-      result += ':' + date.getMinutes();
-      return result;
+
+    // Make sure we have a 13 digit timestamp
+    if ((object + '').length < 13) {
+      var length = (object + '').length;
+      for (var i = 0; i < 13 - length; i++) {
+        object += '0';
+      }
     }
-    return object;
+
+    console.log(object);
+
+    var result, date = new Date();
+    date.setTime(object);
+    result = date.getFullYear();
+    result += '-' + date.getMonth();
+    result += '-' + date.getDate();
+    result += ' ' + date.getHours();
+    result += ':' + date.getMinutes();
+    return result;
   };
 });
